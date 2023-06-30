c6bf6509e37b111f773163a3f283a19791957d8d
250king <838343916@qq.com>
2023-06-30 20:25:42 +0800

    Modify data model

---
+++ src/model/data/activity.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import DateTime
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+from model.base.database import Base
+from model.data.page import Page
+
+
+class Activity(Base):
+    __tablename__ = 'activity'
+
+    id = Column(Integer, primary_key=True, unique=True, index=True, autoincrement=True, nullable=False)
+    name = Column(String, nullable=False)
+    image = Column(String, unique=True, nullable=False)
+    description = Column(String)
+
+    page = relationship(Page)
